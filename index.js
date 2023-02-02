e9764736d22431b650b2becc2b454f3ba97dc0dc
Scott <scotthenry1@gmail.com>
2023-02-02 09:16:33 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return (
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    renderBooks(data);
+  })
+  )
 }
 
 function renderBooks(books) {
